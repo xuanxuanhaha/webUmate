c96f25df974b28c436abeceae9972f83ff2915c7
xuanxuanhaha <zhangqixuan17@outlook.com>
2020-07-10 21:06:25 +1000

    finish home page first version for monitor

--- home/home.component.ts
+++ home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,22 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('.carousel .carousel-item').each(function () {
+      var next = $(this).next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+
+      for (var i = 0; i < 4; i++) {
+        next = next.next();
+        if (!next.length) {
+          next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+      }
+    });
   }
 
 }
